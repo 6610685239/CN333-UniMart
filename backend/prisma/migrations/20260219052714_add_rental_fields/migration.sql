e992616f14369111dfc84bf2ebee76532a48f549
Kittidet Wichaidit <6610685098@student.tu.ac.th>
2026-02-20 04:53:17 +0700

    Last checkpoint

--- backend/prisma/migrations/20260219052714_add_rental_fields/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateEnum
-CREATE TYPE "ProductType" AS ENUM ('SALE', 'RENT');
-
--- AlterTable
-ALTER TABLE "Product" ADD COLUMN     "currentRenterId" INTEGER,
-ADD COLUMN     "isRented" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "rentDueDate" TIMESTAMP(3),
-ADD COLUMN     "rentPrice" DOUBLE PRECISION,
-ADD COLUMN     "type" "ProductType" NOT NULL DEFAULT 'SALE';
